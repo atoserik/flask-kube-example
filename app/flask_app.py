0195859327fd834dd583811c4b41800d32fa9842
Erik Cenci <erik@Eriks-MBP.station>
2020-04-18 19:35:48 +0200

    Added ssl_context into flask_app.py

--- app/flask_app.py
+++ app/flask_app.py
@@ -6,4 +6,4 @@ def hello():
     return "Hello from Python!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int("5555"))
+    app.run(host='0.0.0.0', port=int("5555"), ssl_context=('cert.pem', 'key.pem') )
